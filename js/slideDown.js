7823fce69c973a787529950e83bfea53b6c24bda
Ikko-T <skyblueboy32@gmail.com>
2021-08-27 11:17:19 +0900

    Fixed a slidedown behavier and restyled the images.

--- js/slideDown.js
+++ js/slideDown.js
@@ -5,27 +5,26 @@ $(function () {
 
   let display = false;
 
-  $('.button').on('click', () => {
-    $('.about').slideToggle(500, () => {
-      if (!display) {
-        $(logo).animate({ top: '25.62rem' }, 300);
-        $(button).animate({ top: '24.50rem' }, 300);
-        $(aboutPage).css({
-          position: 'sticky',
-          top: '0',
-        });
-        $(button).text('CLOSE   X');
-        display = true;
-      } else {
-        $(logo).animate({ top: '7.81rem' }, 300);
-        $(button).animate({ top: '1.75rem' }, 300);
-        $(aboutPage).css({
-          position: '',
-          top: '',
-        });
-        $(button).text('ABOUT   ｖ');
-        display = false;
-      }
-    });
+  $(button).on('click', () => {
+    if (!display) {
+      $(logo).animate({ top: '25.62rem' }, 300);
+      $(button).animate({ top: '24.50rem' }, 300);
+      $(aboutPage).css({
+        position: 'sticky',
+        top: '0',
+      });
+      $(button).text('CLOSE   X');
+      display = true;
+    } else {
+      $(logo).animate({ top: '7.81rem' }, 300);
+      $(button).animate({ top: '1.75rem' }, 300);
+      $(aboutPage).css({
+        position: '',
+        top: '',
+      });
+      $(button).text('ABOUT   ｖ');
+      display = false;
+    }
+    $(aboutPage).slideToggle(300);
   });
 });
